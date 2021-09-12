658b9b03db86b1143e2e52b274b2fa17050e8cd5
theophilus <theophilus.ogbolu@gmail.com>
2021-09-12 09:35:50 +0100

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div id="scene">
+	<iframe src="adore.app/main.html" frameborder="0" />
+</div>
+
