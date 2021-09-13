4e707374b35dc227858e3b4ae693a1186022e0ab
theophilus <theophilus.ogbolu@gmail.com>
2021-09-14 00:12:46 +0100

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-/** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
