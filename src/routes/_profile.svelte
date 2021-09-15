ed0b9e6996eda5f10f1096065d1a2109c139af68
theophilus <theophilus.ogbolu@gmail.com>
2021-09-15 23:40:47 +0100

    more shoes

--- src/routes/_profile.svelte
+++ src/routes/_profile.svelte
@@ -1,5 +1,6 @@
 <div class="bg">
-	<div class="circle" />
+	<div class="circle c1" />
+	<div class="circle c2" />
 </div>
 
 <div id="profile">
@@ -39,7 +40,7 @@
 		padding: var(--pad2);
 		padding-bottom: var(--mobileBottom);
 	}
-	
+
 	#profile_item_name {
 		/* pointer-events: auto; */
 		font-size: 2rem;
@@ -48,32 +49,39 @@
 	#profile_item_price {
 		font-size: 2rem;
 	}
-	
+
 	.bg {
 		/* background-color: red; */
-		
+
 		width: min(100%, var(--ms01));
 		margin: auto;
 
 		position: absolute;
-		
+
 		inset: 0;
 		pointer-events: none;
-		
-		z-index: -2;
 
+		z-index: -2;
 	}
 	.circle {
-		--size: min(40vh, 80vw);
-
 		position: absolute;
-		top: calc(50% - calc(var(--size)/2));
-		right: 0;
-		
+
 		background-color: var(--color3);
 		width: var(--size);
 		height: var(--size);
 
 		border-radius: 50%;
+	}
+	.c1 {
+		--size: min(40vh, 80vw);
+
+		top: calc(50% - calc(var(--size) / 2));
+		right: 0;
+	}
+	.c2 {
+		--size: min(10vh, 20vw);
+
+		top: calc(30% - calc(var(--size) / 2));
+		left: 0;
 	}
 </style>
