cd704bfa1cb55d35bc54765dad6a25f066345796
theophilus <theophilus.ogbolu@gmail.com>
2021-09-15 20:21:40 +0100

    profile

--- src/routes/_profile.svelte
+++ src/routes/_profile.svelte
@@ -1,25 +1,47 @@
-<script>
-	let active = true;
-</script>
+<div id="profile">
+	<div class="block">
+		<div id="profile_item_name">Item Name</div>
 
-<div id="profile" class:active />
+		<div id="profile_item_details">
+			<div id="profile_item_price">Item Price</div>
+			<div id="profile_item_desc">
+				Item Description
+				<br /> bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls
+				bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls
+				bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls
+				bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla bls bla bla
+			</div>
+		</div>
+	</div>
+</div>
 
 <style>
 	#profile {
-		/* display: none; */
-
 		position: absolute;
 		inset: 0;
-		z-index: -1;
-
-		/* flex-direction: column;
-		justify-content: center;
-		align-items: center; */
-
-		background-color: rgb(0, 0, 0);
+		pointer-events: none;
+	}
+	:global(#profile) {
+		display: none;
 	}
 
-	/* :global(#profile) { */
-		/* display: none; */
-	/* } */
+	.block {
+		display: flex;
+		flex-direction: column;
+
+		height: 100%;
+		width: min(100%, 800px);
+		margin: auto;
+		padding: var(--pad2);
+		padding-bottom: 80px;
+	}
+
+	#profile_item_name {
+		/* pointer-events: auto; */
+		font-size: 2rem;
+		margin-bottom: auto;
+	}
+	#profile_item_price {
+		font-size: 2rem;
+	}
 </style>
