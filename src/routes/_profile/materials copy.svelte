a786237ccda697d790343f267d3aea84839e3783
theophilus <theophilus.ogbolu@gmail.com>
2021-09-20 10:21:06 +0100

    fix

---
+++ src/routes/_profile/materials copy.svelte
@@ -0,0 +1,16 @@
+<div class="block" id="mat_block">
+	<!-- mat swatch -->
+</div>
+
+<style>
+	.block {
+		position: relative;
+
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+
+		width: fit-content;
+		top: 20px;
+	}
+</style>
--- src/routes/_profile/materials.svelte
+++ src/routes/_profile/materials.svelte
@@ -1,5 +1,14 @@
 <div class="block" id="mat_block">
-	<!-- mat swatch -->
+	<div class="mat" id="mat_1" />
+	<div class="mat" id="mat_2" />
+	<div class="mat" id="mat_3" />
+	<div class="mat" id="mat_4" />
+	<div class="mat" id="mat_5" />
+	<div class="mat" id="mat_6" />
+	<div class="mat" id="mat_7" />
+	<div class="mat" id="mat_8" />
+	<div class="mat" id="mat_9" />
+	<div class="mat" id="mat_10" />
 </div>
 
 <style>
@@ -12,5 +21,50 @@
 
 		width: fit-content;
 		top: 20px;
+	}
+
+	.mat {
+		--size: 50px;
+
+		width: var(--size);
+		height: var(--size);
+
+		background-color: var(--color3);
+
+		border: 3px solid var(--color1);
+		border-radius: 50%;
+
+		pointer-events: auto;
+		cursor: pointer;
+	}
+	:global(#mat_1) {
+		display: none;
+	}
+	:global(#mat_2) {
+		display: none;
+	}
+	:global(#mat_3) {
+		display: none;
+	}
+	:global(#mat_4) {
+		display: none;
+	}
+	:global(#mat_5) {
+		display: none;
+	}
+	:global(#mat_6) {
+		display: none;
+	}
+	:global(#mat_7) {
+		display: none;
+	}
+	:global(#mat_8) {
+		display: none;
+	}
+	:global(#mat_9) {
+		display: none;
+	}
+	:global(#mat_10) {
+		display: none;
 	}
 </style>
